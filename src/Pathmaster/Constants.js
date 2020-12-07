62639a0d72b5dc046743b561ba2a99b5bc3758c8
Eugene Li <lieugenebaba01@gmail.com>
2020-12-07 18:36:52 -0500

    shhh

--- src/Pathmaster/Constants.js
+++ src/Pathmaster/Constants.js
@@ -1,5 +1,5 @@
-export const NUM_ROWS = 38;
-export const NUM_COLS = 85;
+export const NUM_ROWS = 42;
+export const NUM_COLS = 90;
 export const START_NODE_ROW = Math.floor(NUM_ROWS / 2);
 export const START_NODE_COL = Math.floor(NUM_COLS / 4);
 export const FINISH_NODE_ROW = Math.floor(NUM_ROWS / 2);
